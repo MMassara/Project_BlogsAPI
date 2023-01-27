1093efc3a807d3fb644ff936f011d0b762710355
Matheus Massara <matheus.massara@hotmail.com>
2023-01-26 22:50:18 -0300

    Requisito 07

---
+++ src/models/Category.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+        id: {
+            type: DataTypes.INTEGER, 
+            primaryKey: true,
+            autoIncrement:true,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false, 
+        }
+    }, 
+    {
+        timestamps: false,
+        tableName:'category',
+        underscored: true,
+    });
+
+    return Category;
+}
